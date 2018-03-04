f84bf7030eca156eee3b9799164a52ee1b6536b2
Panagiotis Chatzopoulos <cena7795@hotmail.com>
2018-03-04 20:32:54 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -221,8 +221,8 @@ function amounts(e) {
 
 function moveToAmount(alpha) {
 	return function(d) {
-		var centreY = entityCentres[d.entity].y;
-		var centreX = entityCentres[d.entity].x;
+		var centreY = amountCentres[d.entity].y;
+		var centreX = amountCentres[d.entity].x;
 		if (d.value <= 500000){
 			centreX = amountCentres.first.x ;
 			centreY = amountCentres.first.y  -15;
